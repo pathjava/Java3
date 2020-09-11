97b67c5b18ca7afb0c3cbdba15cb15990c377113
Oleg K <severniyman@gmail.com>
2020-09-11 20:43:30 +0300

    task for testing spring

--- src/main/java/task/RunTask.java
+++ src/main/java/task/RunTask.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class RunTask {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("task-context.xmi");
+        ApplicationContext context = new ClassPathXmlApplicationContext("task-context.xml");
         FileTaskRepository fileTask = context.getBean("task", FileTaskRepository.class);
     }
 
