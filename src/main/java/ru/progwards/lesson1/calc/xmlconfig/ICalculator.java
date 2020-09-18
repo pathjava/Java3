446128c2cf226bca79d686429c00d0a8f27e42cd
Oleg K <severniyman@gmail.com>
2020-09-18 20:02:18 +0300

    xml configuration version

---
+++ src/main/java/ru/progwards/lesson1/calc/xmlconfig/ICalculator.java
@@ -0,0 +1,8 @@
+package ru.progwards.lesson1.calc.xmlconfig;
+
+public interface ICalculator {
+    int sum(int a, int b);
+    int diff(int a, int b);
+    int div(int a, int b);
+    int mult(int a, int b);
+}
